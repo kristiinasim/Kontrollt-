4e35da836405653489a2cae28fdb32e4ce57cac5
kristiinasim <33310254+kristiinasim@users.noreply.github.com>
2017-11-16 16:43:35 +0200

    ül. 2 lõpetatud

--- ül.2.py
+++ ül.2.py
@@ -7,7 +7,16 @@ print("Sõnade arv failis on:")
 print(sõnadearv)
 
 #2.2
-print('Nii paljudes sõnades on vähem kui 5 tähte:')
-for item in words:
-        if len(item) > 4:
-          
+
+num_x = 0
+f = open('kttekst.txt','r+')
+for line in f:
+    wordss = line.split()
+for s in wordss:
+    if len(s) < 5:
+        num_x += len(s)
+print("Sõnu, milles on vähem kui 5 tähte on nii palju:")
+print(num_x)
+                        
+
+        
